1ce7019f7e60a8e75901a715e7d547111e62da9b
zsar.daniel <zsar.daniel@C19-TANAR>
2025-03-10 20:14:43 +0100

    Basics

--- 2_14fi_aknakereso_wpf/MainWindow.xaml.cs
+++ 2_14fi_aknakereso_wpf/MainWindow.xaml.cs
@@ -20,6 +20,8 @@ namespace _2_14fi_aknakereso_wpf
     /// </summary>
     public partial class MainWindow : Window
     {
+        int size = 16;
+        int bomb = 160;
         public MainWindow()
         {
             InitializeComponent();
@@ -27,23 +29,33 @@ namespace _2_14fi_aknakereso_wpf
         }
         void Start()
         {
-            Random r = new Random();
-            for (int row = 0; row < 6; row++)
+            for (int i = 0; i < size; i++)
             {
-                for (int col = 0; col < 6; col++)
+                RowDefinition oneRow = new RowDefinition();
+                ColumnDefinition oneCol = new ColumnDefinition();
+                sajt.ColumnDefinitions.Add(oneCol);
+                sajt.RowDefinitions.Add(oneRow);
+            }
+            Random r = new Random();
+            for (int row = 0; row < size; row++)
+            {
+                for (int col = 0; col < size; col++)
                 {
                     Label oneLabel = new Label();
+                    oneLabel.MouseLeftButtonUp += LeftClickEvent;
+                    oneLabel.MouseRightButtonUp += RightClickEvent;
                     sajt.Children.Add(oneLabel);
-                    oneLabel.Content = "";
+                    //azért tag, mert ez nem jelenik meg a UI-on
+                    oneLabel.Tag = "";
                     Grid.SetRow(oneLabel, row);
                     Grid.SetColumn(oneLabel, col);
                 }
             }
             int count = 0;
-            while (count < 10)
+            while (count < bomb)
             {
-                int row = r.Next(0, 6);
-                int col = r.Next(0, 6);
+                int row = r.Next(0, size);
+                int col = r.Next(0, size);
                 //lekérjük a grid elemeit egy collection-be, ezt a típust kiírja amikor rávisszük az egeret a . children-re
                 UIElementCollection labels = sajt.Children;
                 //ugyanaz mint alatta kommentben, csak másként
@@ -51,9 +63,10 @@ namespace _2_14fi_aknakereso_wpf
                 labels.Cast<Label>().ToList().ForEach(item =>
                 {
                     //ha a sor és az oszlop jó, illetve még nincs akna
-                    if (Grid.GetColumn(item) == col && Grid.GetRow(item) == row && item.Content != "Akna")
+                    if (Grid.GetColumn(item) == col && Grid.GetRow(item) == row && item.Tag != "Akna")
                     {
                         item.Content = "Akna";
+                        item.Tag = "Akna";
                         count++;
                     }
                 });
@@ -72,17 +85,95 @@ namespace _2_14fi_aknakereso_wpf
         void LeftClickEvent(object s, EventArgs e)
         {
             Label oneLabel = s as Label;
-            if (oneLabel.Content == "Akna")
+            if (oneLabel.Tag == "Akna")
             {
+                oneLabel.Background = new SolidColorBrush(Colors.IndianRed);
                 MessageBox.Show("Felrobbantál! :(");
+                this.Close();
             }
             else
             {
-                oneLabel.Background = new SolidColorBrush(Colors.LightGreen);
+                //oneLabel.Background = new SolidColorBrush(Colors.LightGreen);
+                SearchLabels(oneLabel);
             }
+        }
+        void SearchLabels(Label originalLabel)
+        {
+            int row = Grid.GetRow(originalLabel);
+            int col = Grid.GetColumn(originalLabel);
+
+            if (originalLabel.Tag == "Akna")
+            {
+                return;
+            }
+
+            originalLabel.Background = new SolidColorBrush(Colors.LightGreen);
+
+            UIElementCollection labels = sajt.Children;
+
+            labels.Cast<Label>().ToList().ForEach(item =>
+            {
+                if (row + 1 < size)
+                {
+                    if (Grid.GetColumn(item) == col && Grid.GetRow(item) == row + 1 && item.Tag != "Akna")
+                    {
+                        if ((item.Background as SolidColorBrush).Color != Colors.LightGreen)
+                        {
+                            SearchLabels(item);
+                        }
+                    }
+                }
+                if (row - 1 >= 0)
+                {
+                    if (Grid.GetColumn(item) == col && Grid.GetRow(item) == row - 1 && item.Tag != "Akna")
+                    {
+                        if ((item.Background as SolidColorBrush).Color != Colors.LightGreen)
+                        {
+                            SearchLabels(item);
+                        }
+                    }
+                }
+                if (col + 1 < size)
+                {
+                    if (Grid.GetColumn(item) == col + 1 && Grid.GetRow(item) == row && item.Tag != "Akna")
+                    {
+                        if ((item.Background as SolidColorBrush).Color != Colors.LightGreen)
+                        {
+                            SearchLabels(item);
+                        }
+                    }
+                }
+                if (col - 1 >= 0)
+                {
+                    if (Grid.GetColumn(item) == col - 1 && Grid.GetRow(item) == row && item.Tag != "Akna")
+                    {
+                        if ((item.Background as SolidColorBrush).Color != Colors.LightGreen)
+                        {
+                            SearchLabels(item);
+                        }
+                    }
+                }
+
+            });
+
+
+
         }
         void RightClickEvent(object s, EventArgs e)
         {
+            Label oneLabel = s as Label;
+            //if (((SolidColorBrush)oneLabel.Background).Color == Colors.LightBlue)
+            if ((oneLabel.Background as SolidColorBrush).Color == Colors.LightBlue)
+            {
+                oneLabel.Background = new SolidColorBrush(Colors.White);
+                oneLabel.MouseLeftButtonUp += LeftClickEvent;
+            }
+            else
+            {
+                oneLabel.Background = new SolidColorBrush(Colors.LightBlue);
+                oneLabel.MouseLeftButtonUp -= LeftClickEvent;
+            }
+
 
         }
     }
